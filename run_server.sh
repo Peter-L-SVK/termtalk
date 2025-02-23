0ef3226e451bd5a3c33cd39a6c5896ad970e9be7
Peter Leukanič <64000980+Peter-L-SVK@users.noreply.github.com>
2025-02-23 18:16:14 +0100

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 SERVER_BIN="server"
 
 # Check if the server binary exists
-if [ ! -f "$SCRIPT_DIR/target/debug/$SERVER_BIN" ]; then
+if [ ! -f "$SCRIPT_DIR/target/release/$SERVER_BIN" ]; then
     echo "Server binary not found. Building the server..."
     (cd "$SCRIPT_DIR" && cargo build --release --bin $SERVER_BIN)
     if [ $? -ne 0 ]; then
